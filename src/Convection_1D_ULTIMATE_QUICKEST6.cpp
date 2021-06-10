19b2d192247e0cdedafdd5b191b7796bf4aa18e5
Hayley Allison <haylis@gfz-potsdam.de>
2021-06-10 18:50:16 +0200

    edited the Convection_1D_ULTIMATE_Quickest6 script to correct an issue which caused the total PSD to increase with time

--- src/Convection_1D_ULTIMATE_QUICKEST6.cpp
+++ src/Convection_1D_ULTIMATE_QUICKEST6.cpp
@@ -5,7 +5,7 @@
  * 
  * Leonard BP (1988) Universal Limiter for transient interpolation modeling of the advective transport equations: the ULTIMATE conservative difference scheme, NASA technical Memorandum 100916 ICOMP-88-11
  *
- * It works somehow, edit with a great care!
+ * It works somehow, edit with a great care! - edited by Hayley to correct and issue with the PSD update. Without this correction, the total PSD will gain and drop periodically with the drift
  *
  * All equations and formulas for these calculations can be found at http://www.hadian.ir/teaching/CompHydr/3.pdf .
  * Mostly coming from 3.5 Simplified Ultimate Quickest strategy from B.P Leonard the Ultimate conservative difference scheme.
@@ -56,7 +56,10 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 		exit(EXIT_FAILURE);
 	}
 
-	double CourNum_f; // Courant number of a face
+	//double CourNum_f; // Courant number of a face
+	//added by hayley for testing
+	Matrix1D < double > CourNum_f(x_size);
+
 
 	double d1, d3, d5, d7, d9;
 	double D2, D4, D6, D8, D10;
@@ -83,9 +86,6 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 	int ig; // ghost points iterator
 	for (int it = 0; it < num_steps; it++) {
 
-		// copy PSD to PSD_new, potentially can use memcpy to speed-up:
-		// memcpy( &PSD_t[gst], &PSD[0], PSD.size_q1 * sizeof( PSD[0] ) );
-		
 		// copying PSD into PSD_t offset by gst, with gst extra spots at the end
 		// the offset (gst) is used in order to wrap values around from the end to the beginning and vice versa for the periodic case
 		for (ix = 0; ix <= x_size-1; ix++)
@@ -145,18 +145,18 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 			// calculate at ix = 0 if the boundary condition is periodic
 			if (ix == 0) { // special case
 				if (x_LBC_type == "BCT_PERIODIC") { // Periodic
-					CourNum_f = (CourNum[x_size-2] + CourNum[0])/2;
+					CourNum_f[ix] = (CourNum[x_size-2] + CourNum[0])/2;
 					//CourNum_f = CourNum[0];
 				} else {
 					continue; // skip to the next ix - we need to calculate at ix = 1 only for periodic conditions
 				}
 			} else {
-				CourNum_f = (CourNum[ix-1] + CourNum[ix])/2;
+				CourNum_f[ix] = (CourNum[ix-1] + CourNum[ix])/2;
 				//CourNum_f = CourNum[ix];
 			}
 
 			// Copy values into PSD_U, PSD_C, and PSD_D to use only these later
-			if (CourNum_f >= 0) { // Page 33
+			if (CourNum_f[ix] >= 0) { // Page 33
 				//PSD_U = PSD[ix-2];
 				//PSD_C = PSD[ix-1];
 				//PSD_D = PSD[ix];
@@ -185,6 +185,9 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 			}
 
 			// From now on, only PSD_D, PSD_U, and PSD_C are in use
+
+			// notes from Hayley: Because we only use PSD_D, PSD_U, and PSD_C, which we set based on the velocity direction - we now no longer need to consider the sign of the velocity - the absolute value is now used. 
+			
 			// Using 3.5 Simplified Ultimate Quickest strategy from B.P Leonard the Ultimate conservative difference scheme
 			// (2) Calculate some useful parameters
 			DEL = PSD_D[1] - PSD_U[1];
@@ -200,10 +203,10 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 //				// not monotionic
 //				PSD_f[ix] = PSD_C;
 //			} else
-			if (true) {
+			//if (true) {
 				// desired order scheme
 				
-				PSD_f[ix] = 0.5 * (PSD_D[1] + PSD_C) - fabs(CourNum_f)/2 * (PSD_D[1] - PSD_C) - (1 - pow(CourNum_f,2))/6 * CURV; // Leonard and Niknafs, 1991, (85)
+				//PSD_f[ix] = 0.5 * (PSD_D[1] + PSD_C) - fabs(CourNum_f)/2 * (PSD_D[1] - PSD_C) - (1 - pow(CourNum_f,2))/6 * CURV; // Leonard and Niknafs, 1991, (85)
 				// note: CURV = PSD_D - 2*PSD_C + PSD_U;
 				//continue;
 
@@ -211,7 +214,7 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 
 				d1 = PSD_C - PSD_D[1]; //A.2
 				// 2 order
-				PSD_f[ix] = PSD_f[ix] + 0.5 * fabs(CourNum_f) * d1;
+				PSD_f[ix] = PSD_f[ix] + 0.5 * fabs(CourNum_f[ix]) * d1;
 
 
 				// Using table A1 to get the coefficients for the odd differences and table A2 for the even differences
@@ -221,51 +224,51 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 
 				if (always_3rd_order) { // !never_3rd_order && (always_3rd_order || (CURVAV < THC1 && GRAD < THG))) {
 					// 3 order - QUICKEST method
-					PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.))/(3*2*1)
+					PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.))/(3*2*1)
 														* (D2 + d3 / 2); // A.16
 
 				} else {
-					// 4 order
-					PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.))/(3*2*1)
-													* (D2 + fabs(CourNum_f)/4 * d3);
+					// 4 order - PSD_f is still second order
+					PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.))/(3*2*1)
+													* (D2 + fabs(CourNum_f[ix])/4 * d3);
 
 					D4 = (PSD_U[2] - 3*PSD_U[1] + 2*PSD_C + 2*PSD_D[1] - 3*PSD_D[2] + PSD_D[3])/2;
 					d5 = PSD_U[2] - 5*PSD_U[1] + 10*PSD_C - 10*PSD_D[1] + 5*PSD_D[2] - PSD_D[3];
 
 					if (always_5rd_order) { // !never_5rd_order && always_5rd_order) {
 						// 5 order
-						PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.)) * (pow(CourNum_f,2) - pow(2.,2.))/(5*4*3*2*1)
+						PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.)) * (pow(CourNum_f[ix],2) - pow(2.,2.))/(5*4*3*2*1)
 														* (D4 + d5 / 2);
 
 					} else {
-						// 6 order
-						PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.)) * (pow(CourNum_f,2) - pow(2.,2.))/(5*4*3*2*1)
-														* (D4 + fabs(CourNum_f)/6 * d5);
+						// 6 order - PSD_f is still 4th order. 
+						PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.)) * (pow(CourNum_f[ix],2) - pow(2.,2.))/(5*4*3*2*1)
+														* (D4 + fabs(CourNum_f[ix])/6 * d5);
 
 						D6 = (PSD_U[3] - 5*PSD_U[2] + 9*PSD_U[1] - 5*PSD_C - 5*PSD_D[1] + 9*PSD_D[2] - 5*PSD_D[3] + PSD_D[4])/2;
 						d7 = PSD_U[3] - 7*PSD_U[2] + 21*PSD_U[1] - 35*PSD_C + 35*PSD_D[1] - 21*PSD_D[2] + 7*PSD_D[3] - PSD_D[4];
 
 						if (always_7rd_order) { // !never_7rd_order && (always_7rd_order || (CURVAV < THC2 && GRAD < THG))) {
 							// 7 order
-							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.)) * (pow(CourNum_f,2) - pow(2.,2.)) * (pow(CourNum_f,2) - pow(3.,2.))/(7*6*5*4*3*2*1)
+							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.)) * (pow(CourNum_f[ix],2) - pow(2.,2.)) * (pow(CourNum_f[ix],2) - pow(3.,2.))/(7*6*5*4*3*2*1)
 															* (D6 + d7 / 2);
 						} else {
-							// I think there is a bug somewhere here for 8-9 orders :(
+							// I think there is a bug somewhere here for 8-9 orders :( <- this comment is not from hayley
 							// 8 order
-							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.)) * (pow(CourNum_f,2) - pow(2.,2.)) * (pow(CourNum_f,2) - pow(3.,2.))/(7*6*5*4*3*2*1)
-															* (D6 + fabs(CourNum_f)/8 * d7);
+							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.)) * (pow(CourNum_f[ix],2) - pow(2.,2.)) * (pow(CourNum_f[ix],2) - pow(3.,2.))/(7*6*5*4*3*2*1)
+															* (D6 + fabs(CourNum_f[ix])/8 * d7);
 
 							// 8 even:  +1 -7 +20 -28 +14  +14  -28 +20 -7 +1
 							// 9 odd: +1 -9 +36 -84 +126 -126 +84 -36 +9 -1
 							D8 = (PSD_U[4] - 7*PSD_U[3] + 20*PSD_U[2] - 28*PSD_U[1] + 14*PSD_C + 14*PSD_D[1] - 28*PSD_D[2] + 20*PSD_D[3] - 7*PSD_D[4] + PSD_D[5])/2;
 							d9 = PSD_U[4] - 9*PSD_U[3] + 36*PSD_U[2] - 84*PSD_U[1] + 126*PSD_C - 126*PSD_D[1] + 84*PSD_D[2] - 36*PSD_D[3] + 9*PSD_D[4] - PSD_D[5];
 							// 9 order
-							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f,2) - pow(1.,2.)) * (pow(CourNum_f,2) - pow(2.,2.)) * (pow(CourNum_f,2) - pow(3.,2.)) * (pow(CourNum_f,2) - pow(4.,2.))/(9*8*7*6*5*4*3*2*1)
+							PSD_f[ix] = PSD_f[ix] + (pow(CourNum_f[ix],2) - pow(1.,2.)) * (pow(CourNum_f[ix],2) - pow(2.,2.)) * (pow(CourNum_f[ix],2) - pow(3.,2.)) * (pow(CourNum_f[ix],2) - pow(4.,2.))/(9*8*7*6*5*4*3*2*1)
 															* (D8 + d9 / 2);
 						}
 					}
 				}
-			}
+			//}
 
 			// Limit the result, so there is no 'waves' next to sharp gradients
 			// also considering peaks - the peaks should remain to be peaks as much as possible
@@ -306,11 +309,11 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 					// not monotionic, or something
 					PSD_f[ix] = PSD_C;
 				} else
-				if (fabs(CourNum_f) > 1e-31	) { // this will not work if velocity is zero
+				if (fabs(CourNum_f[ix]) > 1e-31	) { // this will not work if velocity is zero
 					// cont. from page 48
 					// !!! PSD_REF = PSD_U + (PSD_C - PSD_U)/CourNum[ix]; // (3)
 					// !!! PSD_REF = PSD_U + (PSD_C - PSD_U)/CourNum_f; // (3)
-					PSD_REF = PSD_U[1] + (PSD_C - PSD_U[1]) / fabs(CourNum_f); // (3)
+					PSD_REF = PSD_U[1] + (PSD_C - PSD_U[1]) / fabs(CourNum_f[ix]); // (3)
 
 					if (DEL > 0) { // (5) limit
 						PSD_f[ix] = fmax(PSD_f[ix], PSD_C); // max
@@ -327,7 +330,7 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 
 
 		// update PSD
-		double PSD_r, PSD_l; // values on left and right faces (between (i and i-1), and (i and i+1))
+		double PSD_r, PSD_l, CourNum_r, CourNum_l; // values on left and right faces (between (i and i-1), and (i and i+1))
 		for (ix = 0; ix < x_size-1; ix++) {
 
 			if (ix == 0 && x_LBC_type != "BCT_PERIODIC") { // special case
@@ -335,10 +338,13 @@ bool Convection_1D_ULTIMATE_QUICKEST6( Matrix1D < double > &PSD,
 			}
 
 			// (7)
-			PSD_r = PSD_f[ix+1];
-			PSD_l = PSD_f[ix];
+			PSD_r     = PSD_f[ix+1];
+			CourNum_r = CourNum_f[ix+1];
+			PSD_l     = PSD_f[ix];
+			CourNum_l = CourNum_f[ix];
 
-			PSD[ix] = PSD[ix] - CourNum[ix] * (PSD_r - PSD_l);
+			//PSD[ix] = PSD[ix] - CourNum[ix] * (PSD_r - PSD_l); - old version that makes the total PSD oscillate
+			PSD[ix] = PSD[ix] - (CourNum_r*PSD_r - CourNum_l*PSD_l);
 		}
 		if (x_LBC_type == "BCT_PERIODIC") { // special case
 			PSD[x_size-1] = PSD[0]; // need to update ix == x_size-1 point for periodic;
