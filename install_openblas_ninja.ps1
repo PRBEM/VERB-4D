7d423105d0e7fe3b27e6ceded5f22c6af5736481
Michael Wutzig <michael.wutzig@gmail.com>
2022-12-15 19:49:56 +0100

    added script to install openblas using ninja

--- install_openblas_ninja.ps1
+++ install_openblas_ninja.ps1
@@ -1,13 +1,20 @@
+# check if Ninja is installed
+try {
+    ninja --version
+}
+catch {
+    throw "Ninja build system not found on PATH. Download Ninja binary from https://github.com/ninja-build/ninja/releases and add the location of ninja.exe to your PATH environmental variable."
+}
 # create a build folder and compile OpenBLAS
 $openblas_branch = "release-0.3.0"
-
 Set-Location OpenBLAS
 git checkout $openblas_branch
+
 if (!(Test-Path build)) {
     New-Item build -ItemType Directory
 }
 Set-Location build
-cmake .. -DBUILD_STATIC_LIBS=FALSE -DBUILD_SHARED_LIBS=TRUE -DUSE_THREAD=0 -DUSE_LOCKING=1 -DUSE_OPENMP=1
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=FALSE -DBUILD_SHARED_LIBS=TRUE -DUSE_THREAD=0 -DUSE_LOCKING=1 -DUSE_OPENMP=1 -GNinja
 cmake --build . --target openblas_shared -j --config Release
 
 # copy the OpenBLAS library files to the user's local AppData folder
@@ -17,8 +24,8 @@ if (!(Test-Path $blas_dir)) {
     New-Item $blas_dir -ItemType Directory
 }
 
-Copy-Item lib\RELEASE\openblas.lib $blas_dir
-Copy-Item lib\RELEASE\openblas.dll $blas_dir
+Copy-Item lib\RELEASE\libopenblas.dll.a $blas_dir\openblas.lib
+Copy-Item lib\RELEASE\libopenblas.dll $blas_dir\openblas.dll
 Write-Output "copied OpenBLAS library files to $blas_dir"
 Set-Location ..\..
 
--- install_verb.sh
+++ install_verb.sh
@@ -1,11 +1,3 @@
-# create ~/.local/bin if it does not exist
-local_bin="$HOME/.local/bin"
-if [ ! -d $local_bin ]
-then
-	echo "creating folder $local_bin"
-	mkdir $local_bin
-fi
-
 # create ./build folder if it does not exist
 if [ ! -d ./build ]
 then
@@ -14,18 +6,6 @@ fi
 
 # run cmake and make to compile VERB4D_Solver and install it to ~/.local/bin
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$HOME/.local"
-make -j2
-make install
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
 cd ..
-
-# if ~/.local/bin is not part of the PATH variable append the definition to ~/.bashrc
-case ":$PATH:" in
-    *:$local_bin*)
-    ;;
-    *)
-	echo "adding $local_bin to PATH in .bashrc"
-	export PATH=$local_bin:$PATH
-	echo "export PATH=$local_bin:"'$PATH' >> ~/.bashrc
-    ;;
-esac
