40955736c9c42a59bcd141d4345a3d7d605e5684
Bernhard Haas <bhaas@spacepager3.gfz-potsdam.de>
2023-02-06 11:06:02 +0100

    Fixed bug in CustomDate comparisons. Added to_full_string method.

--- src/CustomDate.cpp
+++ src/CustomDate.cpp
@@ -1,4 +1,5 @@
 #include "CustomDate.h"
+
 #include <cmath>
 #include <ctime>
 #include <iostream>
@@ -8,16 +9,14 @@
 const double CustomDate::matlabd_epoch = 719529.0;
 const int CustomDate::max_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-CustomDate::CustomDate(double matlabd)
-{
+CustomDate::CustomDate(double matlabd) {
     if (matlabd < matlabd_epoch) {
         std::cout << "Input date is smaller than the epoch." << std::endl;
         exit(EXIT_FAILURE);
     }
 
     std::time_t unix_time = static_cast<time_t>(
-        (matlabd - matlabd_epoch) * 24. * 3600. + 0.5
-    );
+        (matlabd - matlabd_epoch) * 24. * 3600. + 0.5);
     std::tm* tm_tmp = gmtime(&unix_time);
 
     year = 1900 + tm_tmp->tm_year;
@@ -28,8 +27,7 @@ CustomDate::CustomDate(double matlabd)
     second = tm_tmp->tm_sec;
 }
 
-CustomDate::CustomDate(const CustomDate& rhs)
-{
+CustomDate::CustomDate(const CustomDate& rhs) {
     second = rhs.second;
     minute = rhs.minute;
     hour = rhs.hour;
@@ -38,8 +36,7 @@ CustomDate::CustomDate(const CustomDate& rhs)
     year = rhs.year;
 }
 
-CustomDate& CustomDate::operator=(const CustomDate& rhs)
-{
+CustomDate& CustomDate::operator=(const CustomDate& rhs) {
     second = rhs.second;
     minute = rhs.minute;
     hour = rhs.hour;
@@ -50,20 +47,18 @@ CustomDate& CustomDate::operator=(const CustomDate& rhs)
     return *this;
 }
 
-CustomDate CustomDate::add_month(int m) const
-{
-
+CustomDate CustomDate::add_month(int m) const {
     CustomDate date_tmp = *this;
 
     date_tmp.month += m;
-    if (date_tmp.month > 12){
+    if (date_tmp.month > 12) {
         date_tmp.year += (date_tmp.month / 12);
         date_tmp.month = date_tmp.month % 12;
     } else if (date_tmp.month <= 0) {
         date_tmp.year += (date_tmp.month - 12) / 12;
         date_tmp.month = 12 - abs(date_tmp.month % 12);
     }
-    
+
     int max_day = max_days[date_tmp.month - 1];
     if (is_leap_year(date_tmp.year) && date_tmp.month == 2) {
         max_day += 1;
@@ -76,8 +71,43 @@ CustomDate CustomDate::add_month(int m) const
     return date_tmp;
 }
 
-std::string CustomDate::to_string() const
-{
+std::string CustomDate::to_full_string() const {
+    std::string str_rep = std::to_string(year);
+
+    if (month < 10) {
+        str_rep += ("0" + std::to_string(month));
+    } else {
+        str_rep += std::to_string(month);
+    }
+
+    if (day < 10) {
+        str_rep += ("0" + std::to_string(day));
+    } else {
+        str_rep += std::to_string(day);
+    }
+
+    if (hour < 10) {
+        str_rep += (" 0" + std::to_string(hour));
+    } else {
+        str_rep += " " + std::to_string(hour);
+    }
+
+    if (minute < 10) {
+        str_rep += (":0" + std::to_string(minute));
+    } else {
+        str_rep += ":" + std::to_string(minute);
+    }
+
+    if (second < 10) {
+        str_rep += (":0" + std::to_string(second));
+    } else {
+        str_rep += ":" + std::to_string(second);
+    }
+
+    return str_rep;
+}
+
+std::string CustomDate::to_string() const {
     std::string str_rep = std::to_string(year);
 
     if (month < 10) {
@@ -95,8 +125,7 @@ std::string CustomDate::to_string() const
     return str_rep;
 }
 
-CustomDate CustomDate::to_bom() const
-{
+CustomDate CustomDate::to_bom() const {
     CustomDate date_tmp = *this;
     date_tmp.day = 1;
     date_tmp.hour = 0;
@@ -106,8 +135,7 @@ CustomDate CustomDate::to_bom() const
     return date_tmp;
 }
 
-CustomDate CustomDate::to_eom() const
-{
+CustomDate CustomDate::to_eom() const {
     CustomDate date_tmp = *this;
     date_tmp.day = date_tmp.eom();
     date_tmp.hour = 23;
@@ -117,16 +145,14 @@ CustomDate CustomDate::to_eom() const
     return date_tmp;
 }
 
-bool CustomDate::is_leap_year(int y)
-{
-    if (((y%4 == 0) && (y%100 != 0)) || (y%400 == 0))
+bool CustomDate::is_leap_year(int y) {
+    if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
         return true;
 
     return false;
 }
 
-int CustomDate::eom() const
-{
+int CustomDate::eom() const {
     int result = max_days[month - 1];
     if (is_leap_year(year) && month == 2) {
         ++result;
@@ -134,34 +160,35 @@ int CustomDate::eom() const
     return result;
 }
 
-template<typename Comparator>
+template <typename Comparator>
 bool compare(const CustomDate& lhs, const CustomDate& rhs, Comparator comp) {
     const auto lhst = std::make_tuple(
         lhs.get_year(), lhs.get_month(), lhs.get_day(), lhs.get_hour(), lhs.get_minute(), lhs.get_second());
     const auto rhst = std::make_tuple(
-        rhs.get_year(), rhs.get_month(), rhs.get_day(), lhs.get_hour(), lhs.get_minute(), lhs.get_second());
+        rhs.get_year(), rhs.get_month(), rhs.get_day(), rhs.get_hour(), rhs.get_minute(), rhs.get_second());
+
     return comp(lhst, rhst);
 }
 
 bool operator==(const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::equal_to<std::tuple<int,int,int,int,int,int>>{});
+    return compare(lhs, rhs, std::equal_to<std::tuple<int, int, int, int, int, int>>{});
 }
 bool operator!=(const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::not_equal_to<std::tuple<int,int,int,int,int,int>>{});
+    return compare(lhs, rhs, std::not_equal_to<std::tuple<int, int, int, int, int, int>>{});
 }
 
-bool operator< (const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::less<std::tuple<int,int,int,int,int,int>>{});
+bool operator<(const CustomDate& lhs, const CustomDate& rhs) {
+    return compare(lhs, rhs, std::less<std::tuple<int, int, int, int, int, int>>{});
 }
 
 bool operator<=(const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::less_equal<std::tuple<int,int,int,int,int,int>>{});
+    return compare(lhs, rhs, std::less_equal<std::tuple<int, int, int, int, int, int>>{});
 }
 
-bool operator> (const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::greater<std::tuple<int,int,int,int,int,int>>{});
+bool operator>(const CustomDate& lhs, const CustomDate& rhs) {
+    return compare(lhs, rhs, std::greater<std::tuple<int, int, int, int, int, int>>{});
 }
 
 bool operator>=(const CustomDate& lhs, const CustomDate& rhs) {
-    return compare(lhs, rhs, std::greater_equal<std::tuple<int,int,int,int,int,int>>{});
+    return compare(lhs, rhs, std::greater_equal<std::tuple<int, int, int, int, int, int>>{});
 }
--- src/CustomDate.h
+++ src/CustomDate.h
@@ -3,29 +3,29 @@
 #include <string>
 
 class CustomDate {
-public:
-
+   public:
     static const double matlabd_epoch;
 
-    CustomDate(int y=0, int m=1, int d=1) 
-        :year(y), month(m), day(d) {}
+    CustomDate(int y = 0, int m = 1, int d = 1)
+        : year(y), month(m), day(d) {}
     CustomDate(double matlabd);
     CustomDate(const CustomDate& rhs);
     CustomDate& operator=(const CustomDate& rhs);
 
-    int get_second() const {return second;}
-    int get_minute() const {return minute;}
-    int get_hour() const {return hour;}
-    int get_day() const {return day;}
-    int get_month() const {return month;}
-    int get_year() const {return year;}
+    int get_second() const { return second; }
+    int get_minute() const { return minute; }
+    int get_hour() const { return hour; }
+    int get_day() const { return day; }
+    int get_month() const { return month; }
+    int get_year() const { return year; }
 
     CustomDate add_month(int add = 1) const;
 
     std::string to_string() const;
-    CustomDate to_bom() const; // to beginning of month
-    CustomDate to_eom() const; // to end of month
-private:
+    std::string to_full_string() const;
+    CustomDate to_bom() const;  // to beginning of month
+    CustomDate to_eom() const;  // to end of month
+   private:
     int year;
     int month;
     int day;
