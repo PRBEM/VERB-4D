bc93c08af54aa48b9b58a7cc75eedd865ab3a9cc
Michael Wutzig <michael.wutzig@gfz-potsdam.de>
2023-05-01 10:30:40 +0200

    removed mat1,mat2 typedef from diffusion_2d_mkl

--- src/Diffusion_2D_MKL.cpp
+++ src/Diffusion_2D_MKL.cpp
@@ -10,11 +10,11 @@
 #include "Diffusion_2D_MKL.hpp"
 
 void initialize_sparse_values(
-        const mat2d& v_grid, const mat2d& k_grid,
+        const Matrix2D<double>& v_grid, const Matrix2D<double>& k_grid,
         BoundaryConditionType v_lower_type, BoundaryConditionType v_upper_type,
         BoundaryConditionType k_lower_type, BoundaryConditionType k_upper_type,
-        const mat2d& Dvv, const mat2d& Dvk, const mat2d& Dkv, const mat2d& Dkk,
-        const mat2d& jacobian, const mat2d& loss, double dt,
+        const Matrix2D<double>& Dvv, const Matrix2D<double>& Dvk, const Matrix2D<double>& Dkv, const Matrix2D<double>& Dkk,
+        const Matrix2D<double>& jacobian, const Matrix2D<double>& loss, double dt,
         std::vector<double>& values)
 {
     int v_size = v_grid.size_q1;
@@ -271,7 +271,7 @@ void initialize_sparse_indices(int v_size, int k_size, std::vector<int>& column_
     rows_csr.push_back(rows_csr.back() + 2);
 }
 
-void initialize_rhs(std::vector<double>& rhs, const Matrix2D<double>& psd, const mat1d& v_lower, const mat1d& v_upper, const mat1d& k_lower, const mat1d& k_upper, const mat2d& source, double dt)
+void initialize_rhs(std::vector<double>& rhs, const Matrix2D<double>& psd, const Matrix1D<double>& v_lower, const Matrix1D<double>& v_upper, const Matrix1D<double>& k_lower, const Matrix1D<double>& k_upper, const Matrix2D<double>& source, double dt)
 {
     rhs.clear();
     rhs.reserve(psd.size_q1 * psd.size_q2);
@@ -294,12 +294,12 @@ void initialize_rhs(std::vector<double>& rhs, const Matrix2D<double>& psd, const
     }
 }
 
-void Diffusion_2D_MKL(mat2d& psd, const mat2d& v_grid, const mat2d& k_grid,
+void Diffusion_2D_MKL(Matrix2D<double>& psd, const Matrix2D<double>& v_grid, const Matrix2D<double>& k_grid,
         BoundaryConditionType v_lower_type, BoundaryConditionType v_upper_type,
         BoundaryConditionType k_lower_type, BoundaryConditionType k_upper_type,
-        const mat1d& v_lower, const mat1d& v_upper, const mat1d& k_lower, const mat1d& k_upper,
-        const mat2d& Dvv, const mat2d& Dvk, const mat2d& Dkv, const mat2d& Dkk,
-        const mat2d& jacobian, const mat2d& loss, const mat2d& source, double dt,
+        const Matrix1D<double>& v_lower, const Matrix1D<double>& v_upper, const Matrix1D<double>& k_lower, const Matrix1D<double>& k_upper,
+        const Matrix2D<double>& Dvv, const Matrix2D<double>& Dvk, const Matrix2D<double>& Dkv, const Matrix2D<double>& Dkk,
+        const Matrix2D<double>& jacobian, const Matrix2D<double>& loss, const Matrix2D<double>& source, double dt,
         sparse_matrix_t* csrA)
 {
     int mat_size = psd.size_q1 * psd.size_q2;
--- src/Diffusion_2D_MKL.hpp
+++ src/Diffusion_2D_MKL.hpp
@@ -4,27 +4,25 @@
 #include "BoundaryConditionType.hpp"
 #include <vector>
 #include "mkl_sparse_qr.h"
-typedef Matrix2D<double> mat2d;
-typedef Matrix1D<double> mat1d;
 
 void initialize_sparse_values(
-    const mat2d& v_grid, const mat2d& k_grid,
+    const Matrix2D<double>d& v_grid, const Matrix2D<double>& k_grid,
     BoundaryConditionType v_lower_type, BoundaryConditionType v_upper_type,
     BoundaryConditionType k_lower_type, BoundaryConditionType k_upper_type,
-    const mat2d& Dvv, const mat2d& Dvk, const mat2d& Dkv, const mat2d& Dkk, 
-    const mat2d& jacobian, const mat2d& loss, double dt,
+    const Matrix2D<double>& Dvv, const Matrix2D<double>& Dvk, const Matrix2D<double>& Dkv, const Matrix2D<double>& Dkk, 
+    const Matrix2D<double>& jacobian, const Matrix2D<double>& loss, double dt,
     std::vector<double>& values
 );
 
 void initialize_sparse_indices(int v_size, int k_size, std::vector<int>& column_indices, std::vector<int>& rows_csr);
-void initialize_rhs(std::vector<double>& rhs, const Matrix2D<double>& psd, const mat1d& v_lower, const mat1d& v_upper, const mat1d& k_lower, const mat1d& k_upper, const mat2d& source, double dt);
+void initialize_rhs(std::vector<double>& rhs, const Matrix2D<double>& psd, const Matrix1D<double>& v_lower, const Matrix1D<double>& v_upper, const Matrix1D<double>& k_lower, const Matrix1D<double>& k_upper, const Matrix2D<double>d& source, double dt);
 void Diffusion_2D_MKL(
-    mat2d& psd, const mat2d& v_grid, const mat2d& k_grid,
+    Matrix2D<double>& psd, const Matrix2D<double>d& v_grid, const Matrix2D<double>d& k_grid,
     BoundaryConditionType v_lower_type, BoundaryConditionType v_upper_type,
     BoundaryConditionType k_lower_type, BoundaryConditionType k_upper_type,
-    const mat1d& v_lower, const mat1d& v_upper, const mat1d& k_lower, const mat1d& k_upper,
-    const mat2d& Dvv, const mat2d& Dvk, const mat2d& Dkv, const mat2d& Dkk, 
-    const mat2d& jacobian, const mat2d& loss, const mat2d& source, double dt,
+    const Matrix1D<double>& v_lower, const Matrix1D<double>& v_upper, const Matrix1D<double>& k_lower, const Matrix1D<double>& k_upper,
+    const Matrix2D<double>& Dvv, const Matrix2D<double>& Dvk, const Matrix2D<double>& Dkv, const Matrix2D<double>& Dkk, 
+    const Matrix2D<double>& jacobian, const Matrix2D<double>& loss, const Matrix2D<double>& source, double dt,
     sparse_matrix_t* csrA
 );
 #endif
